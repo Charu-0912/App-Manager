cf904e6318447a97b89d7422a5af58c3b6e7bd9a
tushar <aloha.openmethods2677@gmail.com>
2020-11-30 17:29:02 +0530

    Test commit from local machine 20201130_2

--- src/test/java/com/appmanager/qa/testcases/ResetPasswordTest.java
+++ src/test/java/com/appmanager/qa/testcases/ResetPasswordTest.java
@@ -4,6 +4,7 @@ import com.appmanager.qa.base.TestBase;
 import com.appmanager.qa.pages.*;
 import com.relevantcodes.extentreports.ExtentTest;
 import org.apache.log4j.Logger;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -66,6 +67,11 @@ public class ResetPasswordTest extends TestBase {
         String validationError = ResetPassword.ResetPassword();
         softAssert.assertEquals(validationError," Your passsword has been reset successfully. ");
 
+    }
+    @AfterMethod
+    public void teardown() {
+
+        driver.quit();
     }
 
 }
